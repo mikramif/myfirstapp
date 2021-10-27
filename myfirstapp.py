8249309db2938f3e27b4506644ce7a5ff280558c
mikramif <89080764+mikramif@users.noreply.github.com>
2021-10-27 23:01:37 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -16,7 +16,7 @@ if readme:
 
     st.write ("For more info, please contact:")
 
-    st.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
+    st.write("<a href=''>Ikram </a>", unsafe_allow_html=True)
 
 option = st.sidebar.selectbox(
     'Select a mini project',
